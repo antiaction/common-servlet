5e787670b5ba3a31556c075d43b31ac5df9e61e4
nclarke <nclarke>
2005-03-16 20:42:51 +0100

    minor http fixes, initial imap, imagefilter

---
+++ src/main/java/com/antiaction/mayhem/httpd/filters/ImageFilter.java
@@ -0,0 +1,168 @@
+/*
+ * Created on 16-03-2005
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package com.antiaction.mayhem.httpd.filters;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.MediaTracker;
+import java.awt.Container;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+import com.sun.image.codec.jpeg.JPEGCodec;
+import com.sun.image.codec.jpeg.JPEGEncodeParam;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Nicholas
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class ImageFilter implements Filter {
+
+	private FilterConfig filterConfig;
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
+	}
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse resp = (HttpServletResponse)response;
+		ServletContext servletContext = filterConfig.getServletContext();
+
+		String pxStr = request.getParameter( "px" );
+		if ( pxStr != null && pxStr.length() != 0 ) {
+			int px = 0;
+			try {
+				px = Integer.parseInt( pxStr );
+
+				String path = req.getPathInfo();
+				String realPath = servletContext.getRealPath( path );
+
+				// debug
+				//System.out.println( path );
+				//System.out.println( realPath );
+
+				int idx = path.lastIndexOf( "/" );
+				//String outfile = path.substring( idx + 1, path.length() );
+				//System.out.println( outfile + ".1" );
+
+				String outfile = realPath + "-" + px;
+
+				File outFile = new File( outfile );
+				if ( !outFile.exists() ) {
+					scaleImage( realPath, 192, 192, outfile, 100 );
+				}
+
+				byte[] bytes = null;
+				bytes = new byte[ 65536 ];
+
+				ServletOutputStream out = resp.getOutputStream();
+				InputStream is = new BufferedInputStream( new FileInputStream( outfile ) );
+				int len;
+				while ( ( len = is.read( bytes, 0, 65536 ) ) != -1 ) {
+					out.write( bytes, 0, len );
+				}
+				is.close();
+
+				out.flush();
+				out.close();
+				return;
+			}
+			catch (NumberFormatException e) {
+			}
+			System.out.println( pxStr );
+		}
+
+		chain.doFilter( request, response );
+	}
+
+	public synchronized void scaleImage(String infile, int destWidth, int destHeight, String outfile, int quality) {
+		// load image from INFILE
+		Image image = Toolkit.getDefaultToolkit().getImage( infile );
+		MediaTracker mediaTracker = new MediaTracker( new Container() );
+		mediaTracker.addImage( image, 0 );
+		try {
+			mediaTracker.waitForID( 0 );
+		}
+		catch (InterruptedException e) {
+		}
+
+		// determine thumbnail size from WIDTH and HEIGHT
+		double thumbRatio = (double)destWidth / (double)destHeight;
+		int imageWidth = image.getWidth( null );
+		int imageHeight = image.getHeight( null );
+		double imageRatio = (double)imageWidth / (double)imageHeight;
+		if ( thumbRatio < imageRatio ) {
+			destHeight = (int)( destWidth / imageRatio );
+		} else {
+			destWidth = (int)( destHeight * imageRatio );
+		}
+
+		// draw original image to thumbnail image object and
+		// scale it to the new size on-the-fly
+		BufferedImage destImage = new BufferedImage( destWidth, destHeight, BufferedImage.TYPE_INT_RGB );
+		Graphics2D graphics2D = destImage.createGraphics();
+		//graphics2D.setRenderingHint( RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR );
+		graphics2D.setRenderingHint( RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC );
+		graphics2D.setRenderingHint( RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY );
+		graphics2D.drawImage( image, 0, 0, destWidth, destHeight, null );
+
+		// save thumbnail image to OUTFILE
+		try {
+			BufferedOutputStream out = new BufferedOutputStream( new FileOutputStream( outfile ) );
+			JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder( out );
+			JPEGEncodeParam param = encoder.getDefaultJPEGEncodeParam( destImage );
+			quality = Math.max( 0, Math.min( quality, 100 ) );
+			param.setQuality( (float)quality / 100.0f, false );
+			encoder.setJPEGEncodeParam( param );
+			encoder.encode( destImage );
+			out.close(); 
+		}
+		catch (FileNotFoundException e) {
+			System.out.println( e );
+		}
+		catch (IOException e) {
+			System.out.println( e );
+		}
+
+		graphics2D.dispose();
+		graphics2D = null;
+
+		image.flush();
+		image = null;
+
+		destImage.flush();
+		destImage = null;
+	}
+
+}
--- src/main/java/com/antiaction/mayhem/httpd/filters/LogFilter.java
+++ src/main/java/com/antiaction/mayhem/httpd/filters/LogFilter.java
@@ -1,3 +1,16 @@
+/*
+ * Asynchronious logging filter.
+ * Copyright (C) 2004, 2005  Nicholas Clarke
+ *
+ */
+
+/*
+ * History:
+ *
+ * 04-Mar-2005 : Refactoring, made storing asynchronious.
+ *
+ */
+
 package com.antiaction.mayhem.httpd.filters;
 
 import javax.servlet.Filter;
@@ -15,19 +28,37 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
 import java.sql.SQLException;
+
 import javax.naming.InitialContext;
 import javax.naming.Context;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 //import javax.sql.ConnectionPoolDataSource;
 
-public class LogFilter implements Filter {
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Asynchronious logging filter.
+ *
+ * @version 1.00
+ * @author Nicholas Clarke <mayhem[at]antiaction[dot]com>
+ */
+public class LogFilter implements Filter, Runnable {
+
+	/** Shutdown boolean. */
+	private boolean exit = false;
 
 	private FilterConfig config;
 	private String dsName;
 	private Context ctx;
 	private DataSource ds;
+
+	private Object syncObj = new Object();
+
+	private List entries = new ArrayList();
 
 	public void init(FilterConfig filterConfig) throws ServletException {
 		config = filterConfig;
@@ -41,6 +72,8 @@ public class LogFilter implements Filter {
 				System.out.println( e );
 			}
 		}
+		Thread t = new Thread( this );
+		t.start();
 	}
 
 	public void destroy() {
@@ -58,58 +91,108 @@ public class LogFilter implements Filter {
 
 		long exitTime = System.currentTimeMillis();
 
-		Connection conn = null;
-		String insertSql;
-		Statement stm = null;
-		ResultSet rs = null;
-		PreparedStatement insertStm;
-		try {
-			if ( ds != null ) {
-				conn = ds.getConnection();
-				if ( conn != null ) {
-					conn.setCatalog( "alfachins" );
-					conn.setAutoCommit( false );
+		String resource = req.getRequestURI();
+		String queryString = req.getQueryString();
+		if ( queryString != null ) {
+			resource += "?" + queryString;
+		}
 
-					insertSql = "";
-					insertSql += "INSERT INTO accesslog";
-					insertSql += "(timestamp, clientIp, referer, method, resourcePath, sessionId, statusCode, processTime) " + "\n";
-					insertSql += "VALUES(?, ?, ?, ?, ?, ?, ?, ?)" + "\n";
-					insertStm = conn.prepareStatement( insertSql, Statement.RETURN_GENERATED_KEYS  );
+		String sessionId = null;
+		if ( httpSession != null ) {
+			sessionId = httpSession.getId();
+		}
 
-					String resource = req.getRequestURI();
-					String queryString = req.getQueryString();
-					if ( queryString != null ) {
-						resource += "?" + queryString;
+		FilterEntry entry = new FilterEntry();
+		entry.timestamp = new Timestamp( enterTime );
+		entry.clientIp = request.getRemoteAddr();
+		entry.referer = req.getHeader( "referer" );
+		entry.method = req.getMethod();
+		entry.resourcePath = resource;
+		entry.sessionId = sessionId;
+		entry.statusCode = 1;
+		entry.processTime = (int)(exitTime - enterTime);
+
+		synchronized ( syncObj ) {
+			entries.add( entry );
+		}
+	}
+
+	class FilterEntry {
+		Timestamp timestamp;
+		String clientIp;
+		String referer;
+		String method;
+		String resourcePath;
+		String sessionId;
+		int statusCode;
+		int processTime;
+	}
+
+	public void run() {
+		while ( !exit ) {
+			try {
+				Thread.sleep( 60 * 1000 );
+
+				List tmpEntries = null;
+				synchronized ( syncObj ) {
+					if ( entries != null && entries.size() > 0 ) {
+						tmpEntries = entries;
+						entries = new ArrayList();
 					}
+				}
 
-					String sessionId = null;
-					if ( httpSession != null ) {
-						sessionId = httpSession.getId();
+				if ( tmpEntries != null && tmpEntries.size() > 0 ) {
+					Connection conn = null;
+					String insertSql;
+					Statement stm = null;
+					ResultSet rs = null;
+					PreparedStatement insertStm;
+
+					try {
+						if ( ds != null ) {
+							conn = ds.getConnection();
+							if ( conn != null ) {
+								conn.setCatalog( "alfachins" );
+								conn.setAutoCommit( false );
+
+								insertSql = "";
+								insertSql += "INSERT INTO accesslog";
+								insertSql += "(timestamp, clientIp, referer, method, resourcePath, sessionId, statusCode, processTime) " + "\n";
+								insertSql += "VALUES(?, ?, ?, ?, ?, ?, ?, ?)" + "\n";
+								insertStm = conn.prepareStatement( insertSql, Statement.RETURN_GENERATED_KEYS  );
+
+								for ( int i=0; i<tmpEntries.size(); ++i ) {
+									FilterEntry entry = (FilterEntry)tmpEntries.get( i );
+
+									insertStm.setTimestamp( 1, entry.timestamp );
+									insertStm.setString( 2, entry.clientIp );
+									insertStm.setString( 3, entry.referer );
+									insertStm.setString( 4, entry.method );
+									insertStm.setString( 5, entry.resourcePath );
+									insertStm.setString( 6, entry.sessionId );
+									insertStm.setInt( 7, entry.statusCode );
+									insertStm.setInt( 8, entry.processTime );
+									insertStm.executeUpdate();
+
+									rs = insertStm.getGeneratedKeys();
+									if ( rs != null & rs.next() ) {
+										//System.out.println( rs.getLong( 1 ) );
+									}
+									insertStm.clearParameters();
+
+									conn.commit();
+								}
+								conn.close();
+							}
+						}
 					}
-
-					insertStm.setTimestamp( 1, new java.sql.Timestamp( System.currentTimeMillis() ) );
-					insertStm.setString( 2, request.getRemoteAddr() );
-					insertStm.setString( 3, req.getHeader( "referer" ) );
-					insertStm.setString( 4, req.getMethod() );
-					insertStm.setString( 5, resource );
-					insertStm.setString( 6, sessionId );
-					insertStm.setInt( 7, 1 );
-					insertStm.setInt( 8, (int)(exitTime - enterTime) );
-					insertStm.executeUpdate();
-
-					rs = insertStm.getGeneratedKeys();
-					if ( rs != null & rs.next() ) {
-						//System.out.println( rs.getLong( 1 ) );
+					catch (SQLException e) {
+						System.out.println( e );
 					}
-					insertStm.clearParameters();
-
-					conn.commit();
-					conn.close();
 				}
 			}
-		}
-		catch (SQLException e) {
-			System.out.println( e );
+			catch (InterruptedException e) {
+			}
 		}
 	}
 
