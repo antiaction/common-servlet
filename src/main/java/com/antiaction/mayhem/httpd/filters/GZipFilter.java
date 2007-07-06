d65e0d3be6f551dfeed8c8551b53c1bb343ea146
nclarke <nclarke>
2007-07-06 06:19:42 +0200

    datasource username/password property used if specified small cleanup

--- src/main/java/com/antiaction/mayhem/httpd/filters/GZipFilter.java
+++ src/main/java/com/antiaction/mayhem/httpd/filters/GZipFilter.java
@@ -13,7 +13,18 @@
 
 package com.antiaction.mayhem.httpd.filters;
 
+import java.io.IOException;
 import java.util.zip.GZIPOutputStream;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Experimental response gzip filter.
@@ -21,12 +32,30 @@ import java.util.zip.GZIPOutputStream;
  * @version 1.00
  * @author Nicholas Clarke <mayhem[at]antiaction[dot]com>
  */
-public class GZipFilter {
+public class GZipFilter implements Filter {
+
+	/** FilterConfig used to obtaint ServletContext. */
+	private FilterConfig filterConfig;
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
+	}
+
+	public void destroy() {
+	}
 
 	/** GZIP OutputStream. */
 	private GZIPOutputStream gzipOut;
 
-//	dres.setHeader( "Content-Encoding", "gzip" );
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse resp = (HttpServletResponse)response;
+		ServletContext servletContext = filterConfig.getServletContext();
+
+		chain.doFilter( request, response );
+	}
+
+	//	dres.setHeader( "Content-Encoding", "gzip" );
 	/*
 				if ( bytes != null ) {
 					contentLength = bytes.length;
